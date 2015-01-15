dc8c2d8fcaeea9abac2097de379079e1f3353eae
Gray Calhoun <gray@clhn.org>
2015-01-15 13:58:58 -0600

    Test code: does recursive_ols return expected results when order is set

--- test/runtests.jl
+++ test/runtests.jl
@@ -16,14 +16,22 @@ errors = Array(Float64, P)
 b1 = recursive_ols(y, x, R)
 @test b1[:,1] == x[1:R,:] \ y[1:R]
 @test b1[:,end] == x[1:end-1,:] \ y[1:end-1]
+## results after reordering too
+shuffle = rand(1:n, n)
+b1_shuffled = recursive_ols(y, x, R, shuffle)
+@test b1_shuffled[:,1] == x[shuffle[1:R],:] \ y[shuffle[1:R]]
+@test b1_shuffled[:,end] == x[shuffle[1:end-1],:] \ y[shuffle[1:end-1]]
 
 ## Make sure that different functions to get the recursive OLS
 ## estimates return the same values.
 recursive_ols!(estimates, y, x)
 @test estimates == b1
-
+recursive_ols!(estimates, y, x, shuffle)
+@test estimates == b1_shuffled
 recursive_ols!(estimates, errors, y, x)
 @test estimates == b1
+recursive_ols!(estimates, errors, y, x, shuffle)
+@test estimates == b1_shuffled
 
 ## Make sure basic bootstraps run
 b = Array(Float64, 199) ## Initialize storage. Each call will
